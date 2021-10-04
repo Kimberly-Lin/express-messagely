6a30a967aa78c2cd74f81d2dbd457be5987cbfff
SARA JENSEN <SSOJENSEN@GMAIL.COM>
2021-10-04 16:22:26 -0700

    adding get, messagesFrom, and messagesTo user model methods

--- models/user.js
+++ models/user.js
@@ -3,8 +3,8 @@
 const bcrypt = require("bcrypt");
 const { NotFoundError } = require("../expressError");
 const db = require("../db");
-
-const BCRYPT_WORK_FACTOR = 12;
+const { BCRYPT_WORK_FACTOR } = require("../config");
+const Message = require("./message");
 
 /** User of the site. */
 
@@ -22,9 +22,10 @@ class User {
                                    first_name,
                                    last_name,
                                    phone,
-                                   join_at)
+                                   join_at,
+                                   last_login_at)
                VALUES
-                 ($1, $2, $3, $4, $5, current_timestamp)
+                 ($1, $2, $3, $4, $5, current_timestamp, current_timestamp)
                RETURNING username, password, first_name, last_name, phone`,
       [username, hashPw, first_name, last_name, phone]);
 
@@ -81,6 +82,24 @@ class User {
    *          last_login_at } */
 
   static async get(username) {
+    const result = await db.query(
+      `SELECT username, 
+              first_name, 
+              last_name,
+              phone, 
+              join_at, 
+              last_login_at
+      FROM users
+      WHERE username = $1`, 
+      [username]
+    );
+    
+    const user = result.rows[0];
+
+    if (!user) {
+      throw new NotFoundError();
+    }
+    return user;
   }
 
   /** Return messages from this user.
@@ -92,6 +111,30 @@ class User {
    */
 
   static async messagesFrom(username) {
+    const messageResults = await db.query(
+      `SELECT id
+      FROM messages
+      WHERE from_username = $1
+      ORDER BY id`, [username]
+    );
+    
+    const messages = messageResults.rows;
+
+    if (!messages[0]) {
+      throw new NotFoundError();
+    }
+
+    const messagesWithRecipientInfo = messages.map(async message => {
+      let messageInfo = await Message.get(message.id);
+      return { id : messageInfo.id, 
+                to_user : messageInfo.to_user, 
+                body : messageInfo.body,
+                sent_at : messageInfo.sent_at, 
+                read_at : messageInfo.read_at,
+                }
+    });
+
+    return await (Promise.all(messagesWithRecipientInfo));
   }
 
   /** Return messages to this user.
@@ -103,6 +146,30 @@ class User {
    */
 
   static async messagesTo(username) {
+    const messageResults = await db.query(
+      `SELECT id
+      FROM messages
+      WHERE to_username = $1
+      ORDER BY id`, [username]
+    );
+    
+    const messages = messageResults.rows;
+
+    if (!messages[0]) {
+      throw new NotFoundError();
+    }
+
+    const messagesWithRecipientInfo = messages.map(async message => {
+      let messageInfo = await Message.get(message.id);
+      return { id : messageInfo.id, 
+                from_user : messageInfo.from_user, 
+                body : messageInfo.body,
+                sent_at : messageInfo.sent_at, 
+                read_at : messageInfo.read_at,
+                }
+    });
+
+    return await (Promise.all(messagesWithRecipientInfo));
   }
 }
 
